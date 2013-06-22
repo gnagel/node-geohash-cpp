7327f6bfc46130c83cb0f91417493717604cad32
Glenn Nagel <glenn@mercury-wireless.com>
2013-06-22 12:48:00 -0400

    Adding new headers for re-named functions and objects

---
+++ src/cgeohash_fn_repeaters.hpp
@@ -0,0 +1,20 @@
+#ifndef _NODE_CGEOHASH_FN_REPEATERS_HPP
+#define _NODE_CGEOHASH_FN_REPEATERS_HPP
+
+#include <node.h>
+#include <string>
+
+namespace cgeohash {
+
+// Node.JS Hooks to GeoHash encoding
+v8::Handle<v8::Value> encode_fn_repeater(const v8::Arguments& args);
+v8::Handle<v8::Value> decode_fn_repeater(const v8::Arguments& args);
+v8::Handle<v8::Value> decode_bbox_fn_repeater(const v8::Arguments& args);
+v8::Handle<v8::Value> neighbor_fn_repeater(const v8::Arguments& args);
+
+// Register the above methods with node
+v8::Handle<v8::Value> register_node_fn_repeaters(v8::Handle<v8::Object> target);
+
+}
+
+#endif /* end hpp */
